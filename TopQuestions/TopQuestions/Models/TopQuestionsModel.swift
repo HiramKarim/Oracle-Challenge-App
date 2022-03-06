98cadd93b0605e08ef9687d7808c7fd6320a84e8
Hiram Castro <hiramcastrodeveloper@gmail.com>
2022-03-05 22:52:03 -0800

    Creating TopQuestions Model

--- TopQuestions/TopQuestions/Models/TopQuestionsModel.swift
+++ TopQuestions/TopQuestions/Models/TopQuestionsModel.swift
@@ -8,12 +8,42 @@
 import Foundation
 
 struct TopQuestionsResponse: Decodable {
-    let topQuestions:[TopQuestions]?
+    let items:[Items]?
 }
 
-struct TopQuestions:Decodable {
+struct Items:Decodable {
     let tags:[String]?
     let owner:Owner?
+    
+    let isAnswered: Bool?
+    let viewCount:Int?
+    let acceptedAnswerID:Int?
+    let answerCount:Int?
+    let score:Int?
+    let lastActivityDate:Int?
+    let creationDate:Int?
+    let lastEditDate:Int?
+    let questionID:Int?
+    let contentLicense:String?
+    let link:String?
+    let title:String?
+    
+    private enum CodingKeys : String, CodingKey {
+        case tags
+        case owner
+        case isAnswered = "is_answered"
+        case viewCount = "view_count"
+        case acceptedAnswerID = "accepted_answer_id"
+        case answerCount = "answer_count"
+        case score
+        case lastActivityDate = "last_activity_date"
+        case creationDate = "creation_date"
+        case lastEditDate = "last_edit_date"
+        case questionID = "question_id"
+        case contentLicense = "content_license"
+        case link
+        case title
+    }
 }
 
 struct Owner:Decodable {
@@ -21,11 +51,15 @@ struct Owner:Decodable {
     let userID: Int?
     let userType: String?
     let profileImage:String?
+    let displayName:String?
+    let link:String?
     
     private enum CodingKeys : String, CodingKey {
         case reputation
         case userID = "user_id"
         case userType = "user_type"
         case profileImage = "profile_image"
+        case displayName = "display_name"
+        case link
     }
 }
